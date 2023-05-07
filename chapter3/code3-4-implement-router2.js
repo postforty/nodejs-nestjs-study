ccd4d87dcc25dedffd0bb7ccd66b9e0d8a3b6bc7
postforty <75767073+postforty@users.noreply.github.com>
2023-05-07 22:57:59 +0900

    [ADD] user 요청에 query 정보 추가하여 동적 응답 처리

---
+++ chapter3/code3-4-implement-router2.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const url = require("url");
+http
+  .createServer((req, res) => {
+    const path = url.parse(req.url, true).pathname; // 패스명 할당
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+
+    if (path === "/user") {
+      user(req, res);
+    } else if (path === "/feed") {
+      feed(req, res);
+    } else {
+      notFound(req, res);
+    }
+  })
+  .listen("3000", () => console.log("라우터를 만들어 보자!"));
+
+// createServer() 리펙터링
+// 라우팅 이후의 처리를 별도의 함수를 만들어서 처리
+// user 요청에 query 정보 추가하여 동적 응답 처리
+const user = (req, res) => {
+  const userInfo = url.parse(req.url, true).query;
+  res.end(`[user] name : ${userInfo.name}, age: ${userInfo.age}`);
+};
+
+const feed = (req, res) => {
+  res.end(`<ul>
+    <li>picture1</li>
+    <li>picture2</li>
+    <li>picture3</li>
+    </ul>`);
+};
+
+const notFound = (req, res) => {
+  res.statusCode = 404;
+  res.end("404 page not found");
+};
