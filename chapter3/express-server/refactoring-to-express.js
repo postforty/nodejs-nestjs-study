a2f0084b7f452f7bdf030ef5d53591451d908218
postforty <75767073+postforty@users.noreply.github.com>
2023-05-08 22:56:51 +0900

    [ADD] 3.6.3 http → express

---
+++ chapter3/express-server/refactoring-to-express.js
@@ -0,0 +1,24 @@
+const url = require("url");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => console.log("익스프레스로 라우터 리팩터링하기"));
+
+app.get("/", (_, res) => res.end("HOME"));
+app.get("/user", user);
+app.get("/feed", feed);
+
+// 함수 호이스팅 위해 function 키워드 사용
+function user(req, res) {
+  const user = url.parse(req.url, true).query;
+  res.json(`[user] name : ${user.name}, age: ${user.age}`);
+}
+
+function feed(_, res) {
+  res.json(`<ul>
+    <li>picture1</li>
+    <li>picture2</li>
+    <li>picture3</li>
+    </ul>`);
+}
