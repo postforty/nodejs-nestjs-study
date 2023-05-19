ce1fa87aef4f3d00fa61f35e6eb1a4e0f61b6522
postforty <75767073+postforty@users.noreply.github.com>
2023-05-19 21:19:27 +0900

    [ADD] 글쓰기 UI 퍼블

--- chapter7/board/app.js
+++ chapter7/board/app.js
@@ -9,8 +9,13 @@ app.set("views", __dirname + "/views"); // __dirname는 node를 실행하는 디
 app.get("/", (req, res) => {
   // home은 템플릿 파일 이름
   res.render("home", {
-    title: "hi!",
+    title: "테스트 게시판",
     message: "express, mongodb, handlebars!",
+  });
+});
+app.get("/write", (req, res) => {
+  res.render("write", {
+    title: "테스트 게시판",
   });
 });
 
