b4987c1b25ed6a40c9519976b046992bcfd8fd4c
postforty <75767073+postforty@users.noreply.github.com>
2023-05-20 19:49:40 +0900

    [ADD] 글쓰기 POST API

--- chapter7/board/app.js
+++ chapter7/board/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const app = express();
+const postService = require("./services/post-service");
+
+// req.body와 POST 요청을 해석하기 위한 설정
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // 몽고디비 연결 함수
 const mongodbConnection = require("./configs/mongodb-connection");
@@ -27,6 +32,12 @@ app.get("/write", (req, res) => {
   res.render("write", {
     title: "테스트 게시판",
   });
+});
+// 글쓰기
+app.post("/write", async (req, res) => {
+  const post = req.body;
+  const result = await postService.writePost(collection, post);
+  res.redirect(`/detail/${result.insertedId}`);
 });
 app.get("/detail/:id", (req, res) => {
   res.render("detail", {
