8ae94302656d0366afa343633ec361fc8352c45c
postforty <75767073+postforty@users.noreply.github.com>
2023-06-01 23:49:26 +0900

    [ADD] 여러 환경 변수 local, dev. prod 환경 변수 생성 - 작동하지 않는 문제 발생

--- chapter9/config-test/src/app.module.ts
+++ chapter9/config-test/src/app.module.ts
@@ -4,8 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
 
+console.log('env : ' + process.env.NODE_ENV);
+console.log('current working directory : ' + process.cwd());
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), WeatherModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
+    }),
+    WeatherModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
