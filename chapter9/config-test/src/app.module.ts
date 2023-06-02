922e0b4d134ef9a965e17f50dd26bfb20e32c315
postforty <75767073+postforty@users.noreply.github.com>
2023-06-02 21:34:01 +0900

    [FIX] 환경 변수에 따라서 다른 환경 변수 파일 사용하도록 설정 수정

--- chapter9/config-test/src/app.module.ts
+++ chapter9/config-test/src/app.module.ts
@@ -6,12 +6,13 @@ import { WeatherModule } from './weather/weather.module';
 
 console.log('env : ' + process.env.NODE_ENV);
 console.log('current working directory : ' + process.cwd());
+console.log(`${process.cwd()}/envs/${process.env.NODE_ENV.trim()}.env`);
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
+      envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV.trim()}.env`,
     }),
     WeatherModule,
   ],
