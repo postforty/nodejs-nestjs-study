6c4f6dfc781fb7802fe48b26ae1852fe05a6fc65
postforty <75767073+postforty@users.noreply.github.com>
2023-06-05 22:20:34 +0900

    [ADD] entity 추가, service 생성

--- chapter10/nest-auth-test/src/user/user.service.ts
+++ chapter10/nest-auth-test/src/user/user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm'; // 리포지토리 주입 데코레이터
+import { Repository } from 'typeorm'; // 리포지토리
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>, // <>안에 entity 객체 추가
+  ) {}
+}
