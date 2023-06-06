c900030e64c67b6a1c41e903f2cd9bb58256d8c2
postforty <75767073+postforty@users.noreply.github.com>
2023-06-06 17:22:23 +0900

    [ADD] 컨트롤러, 서비스 추가 및 테스트

--- chapter10/nest-auth-test/src/user/user.service.ts
+++ chapter10/nest-auth-test/src/user/user.service.ts
@@ -8,4 +8,28 @@ export class UserService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>, // <>안에 entity 객체 타입 추가
   ) {}
+
+  createUser(user): Promise<User> {
+    return this.userRepository.save(user);
+  }
+
+  async getUser(email: string) {
+    const result = await this.userRepository.findOne({
+      where: { email },
+    });
+    return result;
+  }
+
+  async updateUser(email, _user) {
+    const user: User = await this.getUser(email);
+    console.log(_user);
+    user.username = _user.username;
+    user.password = _user.password;
+    console.log(user);
+    this.userRepository.save(user);
+  }
+
+  deleteUser(email: any) {
+    return this.userRepository.delete({ email });
+  }
 }
