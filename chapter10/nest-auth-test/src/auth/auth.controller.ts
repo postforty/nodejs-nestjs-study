3300b1fe46dd819405546fd7f6528ba6061825cb
postforty <75767073+postforty@users.noreply.github.com>
2023-06-07 20:14:05 +0900

    [ADD] 쿠키를 사용한 인증

--- chapter10/nest-auth-test/src/auth/auth.controller.ts
+++ chapter10/nest-auth-test/src/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { CreateUserDto } from 'src/user/user.dto';
 import { AuthService } from './auth.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, Response } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +9,21 @@ export class AuthController {
   @Post('register')
   async register(@Body() userDto: CreateUserDto) {
     return await this.authService.register(userDto);
+  }
+
+  @Post('login')
+  async login(@Request() req, @Response() res) {
+    const userInfo = await this.authService.validateUser(
+      req.body.email,
+      req.body.password,
+    );
+
+    if (userInfo) {
+      res.cookie('login', JSON.stringify(userInfo), {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 24 * 7,
+      });
+    }
+    return res.send({ message: 'login success' });
   }
 }
