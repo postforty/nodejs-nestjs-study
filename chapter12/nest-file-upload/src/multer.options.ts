1084db8b247b0578072e5d8993e81941991812a9
postforty <75767073+postforty@users.noreply.github.com>
2023-06-18 18:11:28 +0900

    [UPDATE] 업로드한 파일을 특정 경로에 저장

---
+++ chapter12/nest-file-upload/src/multer.options.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { randomUUID } from 'crypto';
+import { diskStorage } from 'multer';
+import { extname, join } from 'path';
+
+export const multerOption = {
+  storage: diskStorage({
+    destination: join(__dirname, '..', 'uploads'),
+    filename: (req, file, cb) => {
+      cb(null, randomUUID() + extname(file.originalname));
+    },
+  }),
+};
